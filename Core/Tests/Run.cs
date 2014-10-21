9b02f3ab58e7f2208380c4161580673dc4e6b6ca
Jeremy Hughes <jedahu@gmail.com>
2014-10-22 00:59:50 +1300

    wip: mono bug?

--- Core/Tests/Run.cs
+++ Core/Tests/Run.cs
@@ -1,11 +1,24 @@
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 using Fuchu;
 using Sharper.C.Laws;
 
 namespace Sharper.C.Tests
 {
-    public sealed class Run
+    public class Run
+        : Task
     {
         public static int Main(string[] args)
+        {
+            return new Run().Go();
+        }
+
+        public override bool Execute()
+        {
+            return Go() == 0;
+        }
+
+        private int Go()
         {
             return Law.RunParallel<AllTests>();
         }
